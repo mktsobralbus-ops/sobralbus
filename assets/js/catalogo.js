24cb74392f55d2fd928f38330c23fd6294f1e285
Sobral bus <mktsobralbus@gmail.com>
2026-06-17 16:40:37 -0300

    paginas

--- assets/js/catalogo.js
+++ assets/js/catalogo.js
@@ -14,13 +14,13 @@ const catalogoData = [
     "categoria": "rodoviario",
     "destaque": true,
     "ativo": true,
-    "imagem": "..assets/rodoviario/fzj/A.png",
+    "imagem": "../assets/rodoviario/fzj/A.png",
     "ogImagem": "https://sobralbus.com.br/assets/rodoviario/fzj/A.png",
     "descricao": "O Marcopolo Paradiso New G7 1800 DD 2019/2020, sobre chassi Mercedes-Benz O500 RSDD, oferece conforto, desempenho e excelente estado de conservação. Conta com 12 poltronas leito total no piso inferior e 48 semi leito no superior, além de ar-condicionado Spheros/Valeo, geladeiras e carregadores USB/USB-C em todas as poltronas. Possui manutenção em dia, pneus dianteiros novos, rodas de alumínio e pintura nova na cor laranja. Seu conjunto garante conforto aos passageiros e eficiência operacional. É ideal para turismo, fretamento executivo e viagens rodoviárias de média e longa distância.",
     "specs": [
       {
         "key": "Carroceria",
-        "val": "Marcopolo Paradiso New G7 1800 DD"
+        "val": "Marcopolo Paradiso New G7"
       },
       {
         "key": "Chassi",
@@ -32,11 +32,11 @@ const catalogoData = [
       },
       {
         "key": "Quilometragem",
-        "val": "980.000"
+        "val": "989.000"
       },
       {
         "key": "Lugares",
-        "val": "60"
+        "val": "60 (12 no piso inferior e 48 no piso superior)"
       },
       {
         "key": "Combustível",
@@ -48,7 +48,7 @@ const catalogoData = [
       },
       {
         "key": "Tipo",
-        "val": "Rodoviario"
+        "val": "Rodoviário"
       },
       {
         "key": "Estado",
@@ -63,20 +63,16 @@ const catalogoData = [
         "val": "Sim"
       },
       {
-        "key": "Carregador USB",
-        "val": "Sim, e tipo C"
-      },
-      {
-        "key": "Ar-condicionado",
-        "val": "Sim (Spheros/Valeo)"
-      },
-      {
-        "key": "Andares",
-        "val": "2 Pisos"
-      },
-      {
         "key": "Geladeira",
-        "val": "Sim (nos 2 pisos)"
+        "val": "Sim (nos dois pisos)"
+      },
+      {
+        "key": "Carregador USB",
+        "val": "Sim (e tipo C)"
+      },
+      {
+        "key": "Ar-Condicionado",
+        "val": "Sim (Spheros/Valeo)"
       }
     ],
     "badges": [
@@ -84,27 +80,27 @@ const catalogoData = [
       "Rodoviário"
     ],
     "imagens": [
-      "..assets/rodoviario/fzj/A.png",
-      "..assets/rodoviario/fzj/B.png",
-      "..assets/rodoviario/fzj/C.png",
-      "..assets/rodoviario/fzj/D.png",
-      "..assets/rodoviario/fzj/E.png",
-      "..assets/rodoviario/fzj/F.png",
-      "..assets/rodoviario/fzj/G.png",
-      "..assets/rodoviario/fzj/H.png",
-      "..assets/rodoviario/fzj/I.png",
-      "..assets/rodoviario/fzj/J.png",
-      "..assets/rodoviario/fzj/K.png",
-      "..assets/rodoviario/fzj/L.png",
-      "..assets/rodoviario/fzj/M.png",
-      "..assets/rodoviario/fzj/N.png",
-      "..assets/rodoviario/fzj/O.png",
-      "..assets/rodoviario/fzj/P.png",
-      "..assets/rodoviario/fzj/Q.png",
-      "..assets/rodoviario/fzj/R.png",
-      "..assets/rodoviario/fzj/S.png",
-      "..assets/rodoviario/fzj/T.png"
+      "../assets/rodoviario/fzj/A.png",
+      "../assets/rodoviario/fzj/B.png",
+      "../assets/rodoviario/fzj/C.png",
+      "../assets/rodoviario/fzj/D.png",
+      "../assets/rodoviario/fzj/E.png",
+      "../assets/rodoviario/fzj/F.png",
+      "../assets/rodoviario/fzj/G.png",
+      "../assets/rodoviario/fzj/H.png",
+      "../assets/rodoviario/fzj/I.png",
+      "../assets/rodoviario/fzj/J.png",
+      "../assets/rodoviario/fzj/K.png",
+      "../assets/rodoviario/fzj/L.png",
+      "../assets/rodoviario/fzj/M.png",
+      "../assets/rodoviario/fzj/N.png",
+      "../assets/rodoviario/fzj/O.png",
+      "../assets/rodoviario/fzj/P.png",
+      "../assets/rodoviario/fzj/Q.png",
+      "../assets/rodoviario/fzj/R.png",
+      "../assets/rodoviario/fzj/S.png",
+      "../assets/rodoviario/fzj/T.png"
     ],
-    "whatsapp": "Olá,%20tenho%20interesse%20no%20Rodoviario%20Marcopolo%20Paradiso%20New%20G7%201800%20DD,%20no%20valor%20de%20R$%201.350.000,00.%20Gostaria%20de%20mais%20informações."
+    "whatsapp": "Olá,%20tenho%20interesse%20no%20ônibus%20Rodoviário%20Marcopolo%20Paradiso%20New%20G7%201800%20DD.%20Gostaria%20de%20mais%20informações."
   }
 ];
